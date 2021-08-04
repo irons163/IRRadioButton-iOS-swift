24a94620500a2609c9ad7485833e5d959b460376
Phil <phil@PhildeMac-mini.engenius.login>
2021-08-04 13:15:17 +0800

    Init commit.

---
+++ IRRadioButton-iOS-swift/IRRadioButton_iOS_swift.h
@@ -0,0 +1,18 @@
+//
+//  IRRadioButton_iOS_swift.h
+//  IRRadioButton-iOS-swift
+//
+//  Created by Phil on 2021/5/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRRadioButton_iOS_swift.
+FOUNDATION_EXPORT double IRRadioButton_iOS_swiftVersionNumber;
+
+//! Project version string for IRRadioButton_iOS_swift.
+FOUNDATION_EXPORT const unsigned char IRRadioButton_iOS_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRRadioButton_iOS_swift/PublicHeader.h>
+
+
---
+++ demo/demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Phil on 2021/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
